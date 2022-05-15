88031e1427a199aace8230718344f469a0792a9f
Okan Mert TAN <okanmerttan@gmail.com>
2022-05-15 03:19:28 +0300

    Easy

---
+++ Kolay/Mod_Alma/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+//Ortalama Hesaplama(Mod Alma)
+namespace Proje_1 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****Ortalama Hesaplama****");
+            try
+            {
+            Console.WriteLine("Girmek istediğiniz sayı adeti yazınız : ");
+            int index = int.Parse(Console.ReadLine());
+            int[] sayiadeti = new int[index];
+            
+            Console.Clear();
+
+            System.Console.WriteLine("Lütfen Girmek İstediğiniz Sayıları Giriniz");
+            int top = 0;
+            for (int i = 0; i < index; i++)
+            {
+                System.Console.WriteLine("{0}. Sayıyı Giriniz : ",i);
+                sayiadeti[i] = int.Parse(Console.ReadLine()); 
+                top += sayiadeti[i] ;
+            }
+
+            int ortalama = top/index;
+            System.Console.WriteLine("Girmiş Oduğunuz Sayıların Ortalaması : "+ortalama);
+            }
+            catch (System.Exception)
+            {
+                System.Console.WriteLine("Lütfen Sayısal Bir Değer Giriniz !!!!!");
+            }
+        }
+        
+    }
+    
+    
+}
---
+++ Kolay/Üçgen_Çizimi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Proje_3 
+{
+    class Program //Üçgen Çizimi
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Üçgen Çizme Sistemi");
+            Console.WriteLine("Lütfen üçgenin kaç boyutlu (sayısal bir değer) yapmak istediğinizi yazınız : ");
+
+            try
+            {
+                int sayac = int.Parse(Console.ReadLine());//Üçgenin kaç boyutlu olduğunu giriyoruz.
+
+            for (int i = 0; i < sayac; i++)//Döngüler yardımıyla üçgen oluşturyoruz. 
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+                
+            }
+            }
+            catch (System.Exception)//Eğer ki int bir değer girilmezse konsola mesaj yazdırır.
+            {
+                System.Console.WriteLine("Lütfen sayısal bir değer giriniz!!!!");
+            }
+           Console.ReadKey();
+        }
+    }
+}
